d39e3aafbdf7905376af094cb7fa9fe5c25b6e5f
sivaroyal007 <sivaroyal423@gmail.com>
2023-07-21 11:30:18 +0530

    file system

--- Node-First-App/app.js
+++ Node-First-App/app.js
@@ -1,3 +1,15 @@
 console.log('hello')
 const fs=require('fs');
-fs.writeFileSync('hello.txt',' siva')
+fs.writeFileSync('hello.txt',' siva . \n sai'),
+fs .appendFileSync('./hello.txt','\n\n\nadding extras')
+// read
+fs.readFileSync('./hello.txt',(err,res)=>{
+    if(err) return err;
+    else{
+        const a = res.toString()
+        console.log(a)
+    }
+})
+// deleting
+// fs.unlinkSync('./hello.txt')
+// console.log('deleted')
